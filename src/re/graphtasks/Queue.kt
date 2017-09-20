507e8b0bd23099f68c8ee800f753415661c8dced
ongoni <ongoni.fg@gmail.com>
2017-09-20 16:40:57 +0400

    added queue and stack classes

---
+++ src/re/graphtasks/Queue.kt
@@ -0,0 +1,25 @@
+package re.graphtasks
+
+class Queue<T> {
+
+    private var items: MutableList<T> = mutableListOf()
+
+    fun push(value: T) {
+        items.add(value)
+    }
+
+    fun top(): T? {
+        return items.firstOrNull()
+    }
+
+    fun pop(): T {
+        val item = items.first()
+        items.removeAt(0)
+        return item
+    }
+
+    fun isEmpty() : Boolean {
+        return items.isEmpty()
+    }
+
+}
---
+++ src/re/graphtasks/Stack.kt
@@ -0,0 +1,25 @@
+package re.graphtasks
+
+class Stack<T> {
+
+    private var items: MutableList<T> = mutableListOf()
+
+    fun push(value: T) {
+        items.add(value)
+    }
+
+    fun top(): T? {
+        return items.lastOrNull()
+    }
+
+    fun pop(): T {
+        val item = items.last()
+        items.removeAt(items.lastIndex)
+        return item
+    }
+
+    fun isEmpty() : Boolean {
+        return items.isEmpty()
+    }
+
+}
